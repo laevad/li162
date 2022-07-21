b3db775adad52668b9ae6aca7c5de0c9bba2b31d
laevad <djaldave@gmail.com>
2022-07-21 11:58:29 +0800

    removed welcome

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,12 +22,10 @@
 {{--                        </x-nav-link>--}}
                     </div>
                 @else
-                    <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Home') }}
                     </x-nav-link>
-                    <x-nav-link  :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Products') }}
-                    </x-nav-link>
+
                 @endauth
             </div>
 
