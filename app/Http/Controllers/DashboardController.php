147991af982dc9edee7a1db13f3f3f43a8c7773d
laevad <djaldave@gmail.com>
2022-07-21 12:00:18 +0800

    change num

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ use App\Models\Product;
 class DashboardController extends Controller
 {
     public function index(){
-        $products = Product::latest()->paginate(7);
+        $products = Product::latest()->paginate(5);
         return view('welcome', ['products'=>$products]);
     }
 }
